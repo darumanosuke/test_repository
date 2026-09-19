b3686a0f58e9be9c17d59b485f5016e04c124301
Darumanosuke <daruma12go@gmail.com>
2026-09-19 15:42:36 +0900

    課題提出

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function sum(x, y) {
+    return x + y;
+}
+const a = 10;
+const b = 20;
+console.log(sum(a, b));
+//# sourceMappingURL=index.js.map
